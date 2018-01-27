f8d8da1324551777cfebe4c1b986f8845aa7c772
Sean Galimidi <totalsgalimidi@gmail.com>
2018-01-27 13:42:03 +0200

    InputBoxes Yay!
    InputBoxes Yay!

--- Assets/CanvasScript.cs
+++ Assets/CanvasScript.cs
@@ -7,6 +7,7 @@ public class CanvasScript : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
+        
 
 	}
 	
---
+++ Assets/Scripts/Models/InputBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBox {
+
+    string text;
+    string inputText;
+
+    public InputBox(string inputText, string text)
+    {
+        this.text = text;
+        this.inputText = inputText;
+    }
+
+}
